32d175ee4dd85afed40ad6b84adbde95e3242644
famesensor <famfew25@gmail.com>
2020-01-16 00:34:48 +0700

    Test connection

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-app = express();
+const app = express();
 
+// set port number
 port = process.env.PORT || 3000;
 
-app.listen(port);
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const myconection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "customer",
+    multipleStatements: true
+})
+
+myconection.connect((err) => {
+    if(!err){
+        console.log("Connected");
+    }
+    else {
+        console.log("Fail");
+    }
+
+});
+
+// set port, listen for requests
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
