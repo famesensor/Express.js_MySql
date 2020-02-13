83158a3d32010f6180a7dd5c9b9fc76fc86f28d3
famesensor <famfew25@gmail.com>
2020-02-14 00:19:00 +0700

    Update Structure

--- app.js
+++ app.js
@@ -5,31 +5,13 @@ const mysql = require("mysql");
 const app = express();
 
 // set port number
-port = process.env.PORT || 3000;
+port = process.env.PORT || 5000;
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const myconection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "customer",
-    multipleStatements: true
-})
-
-myconection.connect((err) => {
-    if(!err){
-        console.log("Connected");
-    }
-    else {
-        console.log("Fail");
-    }
-
-});
 
 // set port, listen for requests
 app.listen(port, () => {
---
+++ models/db.connec.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const dbConfig = require('../config/db');
+
+// Create a connection to database
+const connection = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
+});
+
+// Start connection
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Database connect success");
+})
+
+module.exports = connection;
