238954bcb20ce48f56a62d268426649fdcdae629
famesensor <famfew25@gmail.com>
2020-02-23 00:23:30 +0700

    Add config

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mysql = require("mysql");
 
 const app = express();
 
+const connection = require('./models/db.connec');
+const user = require('./routes/api/user');
+
 // set port number
 port = process.env.PORT || 5000;
 
@@ -12,6 +15,8 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/user', user);
 
 // set port, listen for requests
 app.listen(port, () => {
