fbd79fe4fd9c9573a1554cb24831472891fe2e49
famesensor <famfew25@gmail.com>
2020-02-23 19:10:01 +0700

    Update func

--- routes/api/user.js
+++ routes/api/user.js
@@ -18,8 +18,26 @@ router.get('/handle/:id', (req, res) => {
     });
 });
 
-router.delete('/delete/:id', (req, res) => {
+router.post('/adduser', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({ name: "Name field is required"});
+    }
     
+    if (!req.body.email) {
+        return res.status(400).json({ email: "Email field is required"});
+    }
+
+    customer.query('insert into users (name, email) value (?, ?)', [req.body.name, req.body.email], (err, result) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    customer.query('delete from users where id = ?', req.params.id, (err, result) => {
+        if (err) throw err;
+        res.status(200).json({ sucees: true});
+    });
 })
 
 module.exports = router;
